e47ad6a1a3881194e430c816eb08db4892262781
Ravindu Karunathilake <karunath@ualberta.ca>
2023-12-18 18:18:22 -0700

    Added functionality to delete nodes from a binary tree

--- binary_tree.c
+++ binary_tree.c
@@ -17,6 +17,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdbool.h>
 
 /* Tree node definition */ 
 typedef struct node{
@@ -35,6 +36,8 @@ void printInOrder(node_t *);
 void printPostOrder(node_t *);
 void printLevelOrder(node_t *);
 void addNode(node_t *, node_t *);
+node_t *removeNode(node_t *, int);
+_Bool contains(node_t *, int);
 int getOperation(void);
 
 /* Global variables */
@@ -67,7 +70,26 @@ int main(int argc, char *argv[]){
       }break;
 
       case 'R':{
-        //removeNode();
+        int rm_val;
+        printf("Which value do you want to delete: ");
+        scanf("%d", &rm_val);
+        clearIOStream();
+
+        root = removeNode(root, rm_val);
+      }break;
+
+      case 'C':{
+        int val; 
+        printf("Value to check: ");
+        scanf("%d", &val);
+        clearIOStream();
+
+        if(contains(root, val)){
+          printf("The requested value %d is in the tree\n", val);
+        }
+        else{
+          printf("The requested value %d is NOT in the tree\n", val);
+        }
       }break;
 
       case 'Q':{
@@ -75,7 +97,7 @@ int main(int argc, char *argv[]){
       }break;
 
       default:{
-        printf("Invalid operation: %c. Please enter a valid operation!", operation);
+        printf("Invalid operation: %c. Please enter a valid operation!\n", operation);
       }
     }
   }
@@ -148,6 +170,107 @@ void addNode(node_t *new_node, node_t *current){
   }
 }
 
+node_t *removeNode(node_t *head, int val){
+  /* Is the tree empty? */
+  if(root == NULL){
+    puts("Tree is empty. Nothing to remove");
+    return root;
+  }
+  
+  /* Recursive base case */
+  if(head == NULL){
+    return head;
+  }
+
+  /* Navigate the tree and find the 
+   * value to be deleted */
+  if(val < head->data){
+    head->left = removeNode(head->left, val);
+    return head;
+  }
+  else if(val > head->data){
+    head->right = removeNode(head->right, val);
+    return head; 
+  }
+
+  /* We have found the node to be deleted */
+  
+  /* If one child is empty */ 
+  if(head->left == NULL && head->right != NULL){
+    node_t *temp = head->right;
+    free(head);
+    return temp; // Child node is now the new head
+  }
+  else if(head->right == NULL && head->left != NULL){
+    node_t *temp = head->left;
+    free(head);
+    return temp; // Child node is now the new head
+  }
+  /* Both children exist */
+  else{
+    /* Find the leaf node used for replacement */ 
+
+    /* Assume the current node is the parent to a
+     * leaf node */ 
+    node_t *leaf_parent = head;
+
+    /* Assume the right child is the leaf node*/
+    node_t *leaf_node = head->right;
+
+    /* Verify the leaf node assumptions */ 
+    while(leaf_node->left != NULL){ // Assumption is not correct
+      /* Keep going down the tree */ 
+      leaf_parent = leaf_node;
+      leaf_node = leaf_node->left; 
+    }
+
+    if(leaf_parent != head){
+      /* Save the leaf nodes right branch to parent */
+      leaf_parent->left = leaf_node->right;
+    }
+    else{
+      leaf_parent->right = leaf_node->right;
+    }
+
+    head->data = leaf_node->data;
+
+    free(leaf_node);
+    return head; 
+  }
+}
+
+_Bool contains(node_t *current, int value){
+  /* Is the tree empty? */ 
+  if(current == NULL){
+    return false;
+  }
+
+  /* Recursive base case */ 
+  if(value == current->data){
+    return true;
+  }
+
+  /* Check left */
+  else if(value < current->data){
+    if(current->left == NULL){
+      return false; 
+    }
+    else{
+       return contains(current->left, value);
+    }
+  }
+
+  /* Check right */
+  else{
+    if(current->right == NULL){
+      return false;
+    }
+    else{
+      return contains(current->right, value);
+    }
+  }
+}
+
 void printPreOrder(node_t *current){
   /* Recursive base case */ 
   if(current == NULL){
@@ -193,7 +316,7 @@ void getData(int *val){
 }
 
 int getOperation(void){
-  printf("S)how the tree, A)dd to the tree, R)emove from tree, Q)uit: ");
+  printf("S)how the tree, A)dd to the tree, R)emove from tree, C)heck value, Q)uit: ");
   int choice = toupper(getchar());
   clearIOStream();
 
